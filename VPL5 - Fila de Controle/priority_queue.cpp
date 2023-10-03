20b0f06c34f316d736d2ac4d305a9d9ba47dfadf
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-02 21:38:37 -0300

    TEste

---
+++ VPL5 - Fila de Controle/priority_queue.cpp
@@ -0,0 +1,21 @@
+#include "priority_queue.hpp"
+
+void enqueue(int e, unsigned priority){
+    //TODO Something
+    return;
+}
+
+void dequeue(){
+    //TODO AEEE
+    return;
+}
+
+int peek(){
+    //TODO AEEE
+    return 0;
+}
+
+unsigned size(){
+    //TODO AEEE
+    return 0;
+}
