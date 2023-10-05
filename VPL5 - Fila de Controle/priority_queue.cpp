4b867df5441736c88b04799fae0312056cf27e78
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-05 15:48:51 -0300

    Created Makefile

--- VPL5 - Fila de Controle/priority_queue.cpp
+++ VPL5 - Fila de Controle/priority_queue.cpp
@@ -6,28 +6,28 @@ void IntPriorityQueue::enqueue(int e, unsigned priority){
     IntPriorityQueue::dataType element(e, priority);
     std::vector<dataType>::iterator it;
 
-    if (IntPriorityQueue::priorityQueue.size() != 0){
-        for(it = IntPriorityQueue::priorityQueue.begin(); it < IntPriorityQueue::priorityQueue.end(); it++){
+    if (this->priorityQueue.size() != 0){
+        for(it = this->priorityQueue.begin(); it < this->priorityQueue.end(); it++){
             if ( it->second < element.second )
-                IntPriorityQueue::priorityQueue.insert(it, element);
+                this->priorityQueue.insert(it, element);
           }
     }
     else
-        IntPriorityQueue::priorityQueue.push_back(element);
+        this->priorityQueue.push_back(element);
 
     return;
 }
 
 void IntPriorityQueue::dequeue(){
-    if (IntPriorityQueue::priorityQueue.size() != 0)
-        IntPriorityQueue::priorityQueue.pop_back();
+    if (this->priorityQueue.size() != 0)
+        this->priorityQueue.pop_back();
     return;
 }
 
 int IntPriorityQueue::peek(){
-    return IntPriorityQueue::priorityQueue.back().first;
+    return this->priorityQueue.back().first;
 }
 
 unsigned IntPriorityQueue::size(){
-    return IntPriorityQueue::priorityQueue.size();
+    return this->priorityQueue.size();
 }
