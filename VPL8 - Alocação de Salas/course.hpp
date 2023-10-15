3aa2c15a7c8d48f1dd8e72099b77dec24c3128d2
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-15 08:05:22 -0300

    VPL8 - course.hpp comentado

--- VPL8 - Alocação de Salas/course.hpp
+++ VPL8 - Alocação de Salas/course.hpp
@@ -7,5 +7,8 @@ public:
   std::string id;
   std::string name;
 
+  /// @brief Overload de operator == para a classe
+  /// @param other objeto a ser comparado com o objeto sendo chamado
+  /// @return true se códigos são iguais
   bool operator==(Course const &other) const;
 };
