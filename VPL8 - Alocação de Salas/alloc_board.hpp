bcef08154bc63f9a5a18b8e4bb704d87863179ab
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-15 07:56:04 -0300

    VPL8 - alloc_board.hpp comentado

--- VPL8 - Alocação de Salas/alloc_board.hpp
+++ VPL8 - Alocação de Salas/alloc_board.hpp
@@ -8,8 +8,23 @@
 
 class AllocBoard {
 public:
+
+  /// @brief Cria uma nova alocação para uma determinada disciplina
+  /// @param course curso a ser alocado
+  /// @param alloc alocação de horário
+  /// @return true se alocação foi um sucesso, false se não
   bool allocate(Course course, Allocation alloc);
+
+  /// @brief Remove uma alocação de uma disciplina em uma determinada sala, dia e horário
+  /// @param course_id Código do curso
+  /// @param alloc Alocação do horário
+  /// @return true se alocação foi um sucesso, false se não
   bool deallocate(std::string course_id, Allocation alloc);
+
+
+  /// @brief Retona todas as alocações de uma determinada disciplina, ordenadas por dia, horário e sala.
+  /// @param course_id Identificação da disciplina
+  /// @return Todas as alocações de uma determinada disciplina 
   std::vector<Allocation> find_allocations(std::string course_id);
 
 private:
