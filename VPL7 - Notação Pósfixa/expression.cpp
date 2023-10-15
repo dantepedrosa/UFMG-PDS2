87bf8c96141db031bb248a63ac905722b5ca8a02
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-15 20:38:24 -0300

    Implementted expression.hpp

--- VPL7 - Notação Pósfixa/expression.cpp
+++ VPL7 - Notação Pósfixa/expression.cpp
@@ -1,1 +1,53 @@
 #include "expression.hpp"
+#include <string>
+
+Expression::Expression(std::string postfix){
+    this->postfixExpression = postfix;
+}
+
+float Expression::eval(){
+
+    std::string::size_type sz;   // alias of size_t
+    queue.push_back(std::stof(postfixExpression,&sz));
+
+    std::string str;
+    for(auto x : postfixExpression.substr(sz)){
+        if(x == ' '){
+            if(str == "+"){
+
+                int num2 = queue.back();
+                queue.pop_back();
+                int num1 = queue.back();
+                queue.pop_back();
+                queue.push_back(num1 + num2);
+
+            } else if(str == "-"){
+
+                int num2 = queue.back();
+                queue.pop_back();
+                int num1 = queue.back();
+                queue.pop_back();
+                queue.push_back(num1 - num2);
+
+            } else if(str == "*"){
+
+                int num2 = queue.back();
+                queue.pop_back();
+                int num1 = queue.back();
+                queue.pop_back();
+                queue.push_back(num1 * num2);
+
+            } else if(str == "/"){
+
+                int num2 = queue.back();
+                queue.pop_back();
+                int num1 = queue.back();
+                queue.pop_back();
+                queue.push_back(num1 / num2);
+
+            } else
+                queue.push_back(stof(str));
+        }
+    }
+    return queue.front();
+}
--- VPL7 - Notação Pósfixa/expression.hpp
+++ VPL7 - Notação Pósfixa/expression.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Expression {
 public:
   /// @brief Inicializa a expressão no formato pósfixo.
+  /// @param postfix expressão no formato pósfixo.
   Expression(std::string postfix);
 
   /// @return O resultado da expressão.
   float eval();
   
 private:
-  
+  std::string postfixExpression;
+  std::vector<float> queue;
 };
