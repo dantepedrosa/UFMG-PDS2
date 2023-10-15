1f1ac2726ae493dff5a44bead1f2968b78e09d75
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-15 09:35:47 -0300

    course.cpp implementada

--- VPL8 - Alocação de Salas/course.cpp
+++ VPL8 - Alocação de Salas/course.cpp
@@ -1,1 +1,13 @@
 #include "course.hpp"
+
+Course::Course(std::string id, std::string name){
+    this->id = id;
+    this->name = name;
+}
+
+bool Course::operator==(Course const &other) const {
+    if(this->id == other.id)
+        return true;
+    else
+        return false;
+}
--- VPL8 - Alocação de Salas/course.hpp
+++ VPL8 - Alocação de Salas/course.hpp
@@ -7,6 +7,11 @@ public:
   std::string id;
   std::string name;
 
+  /// @brief Contrutor da classe Course
+  /// @param id Identificação do curso (única)
+  /// @param name Nome completo do curso
+  Course(std::string id, std::string name);
+
   /// @brief Overload de operator == para a classe
   /// Compara se o id do curso é igual
   /// @param other objeto a ser comparado com o objeto sendo chamado
