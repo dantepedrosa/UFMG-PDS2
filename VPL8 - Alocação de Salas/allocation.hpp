438ad0229b3014687804363b651a586cc9fc89d5
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-15 08:10:57 -0300

    allocation.hpp e course.hpp comentados

--- VPL8 - Alocação de Salas/allocation.hpp
+++ VPL8 - Alocação de Salas/allocation.hpp
@@ -8,6 +8,15 @@ public:
   std::string hour;
   std::string room;
 
+  /// @brief Overload de operator == para a classe. 
+  /// Compara se o dia, hora e sala são iguais. 
+  /// @param other classe a ser comparada com a atual
+  /// @return true se o dia, horário e sala são todos iguais
   bool operator==(Allocation const &other) const;
+
+  /// @brief Overload de operator == para a classe.
+  /// Compara se o dia, horário e sala são menores que other.
+  /// @param other classe a ser comparada a atual
+  /// @return true se tanto o dia, horário e sala são menores
   bool operator<(Allocation const &other) const;
 };
--- VPL8 - Alocação de Salas/course.hpp
+++ VPL8 - Alocação de Salas/course.hpp
@@ -8,6 +8,7 @@ public:
   std::string name;
 
   /// @brief Overload de operator == para a classe
+  /// Compara se o id do curso é igual
   /// @param other objeto a ser comparado com o objeto sendo chamado
   /// @return true se códigos são iguais
   bool operator==(Course const &other) const;
